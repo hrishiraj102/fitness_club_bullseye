04901e151a458c76de758adb7f946545d2d39a86
Hrishiraj Sawan <hrishiraj102@gmail.com>
2023-02-11 12:57:54 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -10,16 +10,13 @@ function App() {
     <div   >
       <Grid sx={{display:"flex", justifyContent:"center",ml:"450px",mt:"15%",flexDirection:"column"}}>
       <Typography variant="h4" color="white" sx={{display:"flex",ml:10,mb:3}} >Fitness Club Registration</Typography>
-      <Box sx={{display:"flex", justifyContent:"center", backgroundColor:"white",width:"550px"}}>
+      <Box sx={{display:"flex", justifyContent:"center",width:"550px"}}>
       
       <FitnessClub/>
 
       
 
       </Box>
-      <Button type="submit" variant="contained" sx={{mt:2,mb:2,ml:20,width:"250px"}} color="primary">
-        View Member List
-      </Button>
       </Grid>
       
 
--- src/components/FitnessClub.jsx
+++ src/components/FitnessClub.jsx
@@ -1,60 +1,82 @@
-import { Button, Grid, TextField } from "@mui/material";
+import { Button, Grid, TextField, Box } from "@mui/material";
 import React, { useState } from "react";
 
 
+
 const FitnessClub = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [address, setAddress] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [address, setAddress] = useState("");
+    const [members, setMembers] = useState([]);
+    const [showMembers, setShowMembers] = useState(false);
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    
-  };
+    const handleSubmit = event => {
+        event.preventDefault();
+        setMembers([...members, { name, email, phoneNumber, address }]);
+        alert("User added successfully");
+    };
 
-  return (
-    <Grid width={"80%" }>
-    <form onSubmit={handleSubmit}>
-      <TextField 
-        sx={{mt:2}}
-        label="Name"
-        value={name}
-        onChange={event => setName(event.target.value)}
-        fullWidth
-        required
-        margin="normal"
-      />
-      <TextField
-        label="Email"
-        value={email}
-        onChange={event => setEmail(event.target.value)}
-        required
-        fullWidth
-        margin="normal"
-      />
-      <TextField
-        label="Phone Number"
-        value={phoneNumber}
-        onChange={event => setPhoneNumber(event.target.value)}
-        required
-        margin="normal"
-        fullWidth
-      />
-      <TextField
-        label="Address"
-        value={address}
-        onChange={event => setAddress(event.target.value)}
-        fullWidth
-        
-        margin="normal"
-      />
-      <Button type="submit" variant="contained" sx={{mt:2,mb:2,ml:20}} color="primary">
-        Add Member
-      </Button>
-    </form>
-    </Grid>
-  );
+    const toggleShowMembers = () => {
+        setShowMembers(!showMembers);
+    };
+
+    return (
+        <Box>
+            <Grid width={"100%"} sx={{ backgroundColor: "white" }}>
+                <Grid width={"70%"} marginLeft={8}>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            sx={{ mt: 2 }}
+                            label="Name"
+                            value={name}
+                            onChange={event => setName(event.target.value)}
+                            fullWidth
+                            required
+                            margin="normal"
+                        />
+                        <TextField
+                            label="Email"
+                            value={email}
+                            onChange={event => setEmail(event.target.value)}
+                            required
+                            fullWidth
+                            margin="normal"
+                        />
+                        <TextField
+                            label="Phone Number"
+                            value={phoneNumber}
+                            onChange={event => setPhoneNumber(event.target.value)}
+                            required
+                            margin="normal"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Address"
+                            value={address}
+                            onChange={event => setAddress(event.target.value)}
+                            fullWidth
+
+                            margin="normal"
+                        />
+                        <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2, ml: 16 }} color="primary" >
+                            Add Member
+                        </Button>
+                    </form>
+                </Grid>
+
+
+
+            </Grid>
+            <Grid>
+                <Button type="submit" variant="contained" sx={{ mt: 2, mb: 2, ml: 18, width: "250px" }} color="primary"
+                    onClick={toggleShowMembers}
+                >
+                    View Member List
+                </Button>
+            </Grid>
+        </Box>
+    );
 };
 
 export default FitnessClub;
